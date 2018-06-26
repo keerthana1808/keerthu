8a9f8c17cf84cde386ce03e77ef7d9214699209f
keerthana1808 <40415808+keerthana1808@users.noreply.github.com>
2018-06-26 09:37:20 +0530

    Create Limit.java

---
+++ Limit.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Limit
+{
+    public static void main(String args[])
+    {
+     Scanner sc=new Scanner(System.in);
+     int s1=sc.nextInt();
+     int s2=sc.nextInt();
+     int flag=0;
+     for(int i=s1;i<=s2;i++)
+     {
+         for(int j=2;j<i;j++)
+         {
+             if(i%j==0)
+             {
+                 flag=0;
+                 break;
+             }
+             else{
+                 flag=1;
+             }
+         }
+         if(flag==0)
+     {
+         System.out.println(i);
+     }
+     }
+    }            
+            
+}           
+            
+            
+            
+            
+
+
+
