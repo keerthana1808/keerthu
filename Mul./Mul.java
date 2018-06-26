a536d57dc87024964cfd9bb015353b6962cf5118
keerthana1808 <40415808+keerthana1808@users.noreply.github.com>
2018-06-26 14:05:18 +0530

    Create Mul.java

---
+++ Mul./Mul.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Mul
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=1;
+        for(int i=1;i<=n;i++)
+        {
+         a=n*i;   
+        
+        System.out.print(a+" ");
+    }
+    }
+}
